02b15cad9ab6d109b562d5f1f70e3067b98774eb
Emiel Molenaar <emiel.molenaar@gmail.com>
2016-03-03 14:47:07 +0100

    Fixes command checking; shell_exec returns the output from the executed command or NULL if an error occurred or the command produces no output.

--- Ansible/Ansible.php
+++ Ansible/Ansible.php
@@ -127,7 +127,7 @@ final class Ansible
     {
         // normally ansible is in /usr/local/bin/*
         if ('' === $command) {
-            if (true === shell_exec('which ' . $default)) {
+            if (shell_exec('which ' . $default)) {
                 $command = $default;
             } else { // not testable without ansible installation
                 throw new CommandException('No ' . $default . ' executable present in PATH!');
